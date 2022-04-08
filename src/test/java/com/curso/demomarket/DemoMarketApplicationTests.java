99821422b8cfda37d13f603e3d294cfd41561288
Fabio Cordoba <fabiocordoba1@gmail.com>
2022-04-08 12:23:45 -0500

    inicial

---
+++ src/test/java/com/curso/demomarket/DemoMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.demomarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
