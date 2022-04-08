3b4f275abc5748f881f55e1c7913c9e20a374cc0
Fabio Cordoba <fabiocordoba1@gmail.com>
2022-04-08 12:52:37 -0500

    Configuration of proyect

--- src/main/java/com/curso/demomarket/DemoMarketApplication.java
+++ src/main/java/com/curso/demomarket/DemoMarketApplication.java
@@ -1,4 +1,4 @@
-package com.curso.demomarket;
+ package com.curso.demomarket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
